4be70495d3ba7ba5c2ba85d84d1a1eca3fcf6794
Anatolii Lishchynskyi <anatolii.lishchynskyi@cossacklabs.com>
2020-08-26 15:49:52 +0300

    Update Android example
    * Update gradle to 6.6.1
    * In `build.gradle`:
      * Update com.android.tools.build to 4.0.1
      * Update Themis to 0.13.1
      * Remove cossacklabs maven repository since
        Themis is now available at jcenter
    * Rewrite Secure Cell example according to new API

--- android-example/app/src/main/java/com/cossacklabs/themis/android/example/MainActivitySecureCell.java
+++ android-example/app/src/main/java/com/cossacklabs/themis/android/example/MainActivitySecureCell.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import com.cossacklabs.themis.InvalidArgumentException;
 import com.cossacklabs.themis.NullArgumentException;
 import com.cossacklabs.themis.SecureCell;
-import com.cossacklabs.themis.SecureCellData;
 import com.cossacklabs.themis.SecureCellException;
 
 import java.nio.charset.Charset;
@@ -33,21 +32,17 @@ public class MainActivitySecureCell extends AppCompatActivity {
     void encryptDataForStoring() throws SecureCellException, NullArgumentException, InvalidArgumentException {
         Charset charset = StandardCharsets.UTF_8;
         String pass = "pass";
-        byte[] passKey = pass.getBytes(charset);
-
         String message = "hello message";
-        byte[] context = null;
 
-        SecureCell sc = new SecureCell(passKey, SecureCell.MODE_SEAL);
+        SecureCell.Seal sc = SecureCell.SealWithPassphrase(pass, charset);
 
-        SecureCellData protectedData = sc.protect(passKey, context, message.getBytes(charset));
-        String encodedString = Base64.encodeToString(protectedData.getProtectedData(), Base64.NO_WRAP);
+        byte[] protectedData = sc.encrypt(message.getBytes(charset));
+        String encodedString = Base64.encodeToString(protectedData, Base64.NO_WRAP);
         Log.d("SMC", "encrypted string = " + encodedString);
 
         byte[] decodedString = Base64.decode(encodedString, Base64.NO_WRAP);
-        SecureCellData protectedDataAgain = new SecureCellData(decodedString, null);
 
-        byte[] unprotected = sc.unprotect(passKey, context, protectedDataAgain);
+        byte[] unprotected = sc.decrypt(decodedString);
         String decryptedData = new String(unprotected, charset);
         Log.d("SMC", "decrypted data = " + decryptedData);
     }
